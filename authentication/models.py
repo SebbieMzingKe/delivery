93677cc2503042b9baf82c5c26d052ef3875d75a
SebbieMzingKe <sebbievilar2@gmail.com>
2025-01-23 14:22:14 +0300

    chore: modify models

--- authentication/models.py
+++ authentication/models.py
@@ -22,7 +22,7 @@ class CustomUserManager(BaseUserManager):
 
         new_user = self.model(email=email, **extra_fields)
 
-        new_user.setpassword(password)
+        new_user.set_password(password)
 
         new_user.save()
 
@@ -55,6 +55,8 @@ class User(AbstractUser):
     USERNAME_FIELD='email'
 
     REQUIRED_FIELDS=['username', 'phone_number']
+
+    objects=CustomUserManager()
 
     def __str__(self):
         return f"<User {self.email}"
