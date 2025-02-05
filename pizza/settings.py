04a710fb544f98c2b7cb85b9920e62ef0f59cc70
SebbieMzingKe <sebbievilar2@gmail.com>
2025-02-05 08:59:05 +0300

    chore: modify settings

--- pizza/settings.py
+++ pizza/settings.py
@@ -2,6 +2,12 @@ from decouple import config
 from pathlib import Path
 from datetime import timedelta
 
+import os
+from dotenv import load_dotenv
+from urllib.parse import urlparse
+
+load_dotenv()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -15,7 +21,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://delivery-ten-nu.vercel.app/', 'localhost']
 
 
 # Application definition
@@ -101,12 +107,6 @@ WSGI_APPLICATION = 'pizza.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-# Add these at the top of your settings.py
-import os
-from dotenv import load_dotenv
-from urllib.parse import urlparse
-
-load_dotenv()
 
 
 tmpPostgres = urlparse(os.getenv("DATABASE_URL"))
@@ -165,6 +165,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
